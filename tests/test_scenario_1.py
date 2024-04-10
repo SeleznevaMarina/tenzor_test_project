8df446c34b295d3dcc78a8d506628453cfde158b
«Selezneva <petrukhina.marina2011@yandex.ru>
2024-04-10 16:53:57 +0300

    succesfull scenario_1

--- tests/test_scenario_1.py
+++ tests/test_scenario_1.py
@@ -1,23 +1,26 @@
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 from selenium import webdriver
-from page_objects import MainPage, ContactsPage
+from selenium.webdriver.chrome.service import Service
+from page_objects import MainPage, ContactsPage, TenzorPage
 
 
 @pytest.fixture
 def browser():
-    # driver = webdriver.Chrome()
-    driver = webdriver.Chrome(ChromeDriverManager().install())
+    chrome_service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=chrome_service)
     yield driver
     driver.quit()
 
 def test_scenario_1(browser):
     main_page = MainPage(browser)
+    main_page.open()
     main_page.go_to_contacts()
 
     contacts_page = ContactsPage(browser)
     contacts_page.find_tensor_banner()
-    contacts_page.verify_tensor_block()
-    contacts_page.go_to_details()
-    contacts_page.verify_tensor_url()
-    contacts_page.verify_photo_dimensions()
+    tenzor_page = TenzorPage(browser)
+    tenzor_page.verify_tensor_block()
+    tenzor_page.go_to_details()
+    tenzor_page.verify_tensor_url()
+    tenzor_page.verify_photo_dimensions()
