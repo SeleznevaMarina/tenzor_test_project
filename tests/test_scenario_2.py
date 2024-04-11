449e7535984c96ac547fbbd0a2504529859e27a9
«Selezneva <petrukhina.marina2011@yandex.ru>
2024-04-11 16:02:00 +0300

    successful scenario_2

--- tests/test_scenario_2.py
+++ tests/test_scenario_2.py
@@ -12,14 +12,11 @@ def browser():
     yield driver
     driver.quit()
 
-def test_scenario_1(browser):
+def test_scenario_2(browser):
     main_page = MainPage(browser)
     main_page.open()
     main_page.go_to_contacts()
     contacts_page = ContactsPage(browser)
-    contacts_page.find_tensor_banner()
-    tenzor_page = TenzorPage(browser)
-    tenzor_page.verify_tensor_block()
-    tenzor_page.go_to_details()
-    tenzor_page.verify_tensor_url()
-    tenzor_page.verify_photo_dimensions()
+    contacts_page.check_region_and_partners()
+    contacts_page.change_region('Камчатский край')
+    contacts_page.verify_region_change('Камчатский край')
